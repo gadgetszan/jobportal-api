0e234ee7689661f5889491965c947b032200f571
williamchangco26 <williamchangco26@gmail.com>
2021-01-27 16:39:35 +0800

    test2

---
+++ src/test/java/com/gadgetszan/jobportal/Test.java
@@ -0,0 +1,11 @@
+package com.gadgetszan.jobportal;
+
+public class Test
+{
+    public static void main(String[] args)
+    {
+        //This is a test
+        System.out.println("Test commit");
+
+    }
+}
