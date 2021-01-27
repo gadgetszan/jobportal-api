e26349b4984eae96aa3f68861d1de7f74d28e407
rudyl <rudyleetuprio@gmail.com>
2021-01-27 17:25:51 +0800

    test

--- src/main/java/com/gadgetszan/jobportal/dao/UserRepository.java
+++ src/main/java/com/gadgetszan/jobportal/dao/UserRepository.java
@@ -2,7 +2,7 @@ package com.gadgetszan.jobportal.dao;
 
 import com.gadgetszan.jobportal.exceptions.JpAuthException;
 import com.gadgetszan.jobportal.model.User;
-
+//test
 public interface UserRepository {
     Integer create(String firstName,String lastName,String email,String password,String userType)
             throws JpAuthException;
