ed048832ae8ae0bd22526d085bdb015c6e509c01
Rolan C. Daniel <rolancdaniel@gmail.com>
2021-01-27 12:11:03 +0800

    Test Commit

--- src/main/java/com/gadgetszan/jobportal/Constants.java
+++ src/main/java/com/gadgetszan/jobportal/Constants.java
@@ -3,5 +3,7 @@ package com.gadgetszan.jobportal;
 public class Constants {
     public static final String API_SECRET_KEY = "jobportalapikey";
     public static final long TOKEN_VALIDITY = 2 * 60 * 60 * 1000;
-
+    /*
+    * modifying this class for github testing
+     */
 }
